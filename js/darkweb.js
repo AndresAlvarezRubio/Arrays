59b79574efb84fd876f3d7c0acb5487086dc68a2
Andrés Álvarez <andresalvarezrubio@gmail.com>
2024-06-07 15:24:38 +0200

    arreglo imagen-video

--- js/darkweb.js
+++ js/darkweb.js
@@ -1,6 +1,7 @@
 const SWITCH = document.getElementById("switch");
 let body = document.querySelector("body");
 let imageChange = document.querySelector(".container-video");
+let imageContainer = document.querySelector("figure");
 
 
 
@@ -10,7 +11,7 @@ SWITCH.addEventListener("click", e => {
 
 })
 
-imageChange.addEventListener("click", e => {
+imageContainer.addEventListener("click", e => {
 
     imageChange.innerHTML ='<iframe src="https://www.youtube.com/embed/7j8ySSs_yww" title="Chase Status - Weed Rum (ft. Masicka)" frameborder="0" referrerpolicy="strict-origin-when-cross-origin"></iframe>';
 
